5e4b80e5ebf264a82f0b80d91fdb93d38a920d19
SamuelGonzalez780 <120437981+SamuelGonzalez780@users.noreply.github.com>
2024-11-22 08:28:06 -0600

    Add files via upload

--- Tools/ctrl.py
+++ Tools/ctrl.py
@@ -3,18 +3,33 @@ from tkinter import PhotoImage
 from PIL import Image, ImageTk
 
 
-def loadImages(filePath, size):
+def loadIcon(filePath, size):
     imgTemp = Image.open(filePath)
     imgTemp = imgTemp.resize((size, size), Image.LANCZOS)
     phiTem = ImageTk.PhotoImage(imgTemp)
     return phiTem
 
 
-'''
-root=CTk()
+def loadImagesCustom(filePath, width, height):
+    imgTemp = Image.open(filePath)
+    imgTemp = imgTemp.resize((width, height), Image.LANCZOS)
+    phiTem = ImageTk.PhotoImage(imgTemp)
+    return phiTem
 
-lbl=CTkButton(root, text="",image=loadImages("../Media/show.png",20)).pack()
 
-root.mainloop()
+def deleteContent(frame):
+    for widget in frame.winfo_children():
+        widget.destroy()
 
-'''
+
+
+def deleteElement(self):
+    self.destroy()
+
+
+def increase(btn, color, image):
+    btn.configure(width=150, height=60, fg_color=color, text_color='#f5f6fa', image=loadIcon(image, 40))
+
+
+def decrease(btn, color, image):
+    btn.configure(width=120, height=50, fg_color=color, text_color='#1e272e', image=loadIcon(image, 40))
