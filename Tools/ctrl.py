0e576703f7cf766f0fc638bfd4a86212c9b1095f
SamuelGonzalez780 <120437981+SamuelGonzalez780@users.noreply.github.com>
2024-11-26 11:36:15 -0600

    Add files via upload

---
+++ Tools/ctrl.py
@@ -0,0 +1,66 @@
+from customtkinter import *
+from tkinter import *
+from tkinter import messagebox
+from PIL import Image, ImageTk
+import os
+import shutil
+
+
+def loadIcon(filePath, size):
+    imgTemp = Image.open(filePath)
+    imgTemp = imgTemp.resize((size, size), Image.LANCZOS)
+    phiTem = ImageTk.PhotoImage(imgTemp)
+    return phiTem
+
+
+def loadImagesCustom(filePath, width, height):
+    imgTemp = Image.open(filePath)
+    imgTemp = imgTemp.resize((width, height), Image.LANCZOS)
+    phiTem = ImageTk.PhotoImage(imgTemp)
+    return phiTem
+
+
+def deleteContent(frame):
+    for widget in frame.winfo_children():
+        widget.destroy()
+
+
+def deleteElement(element):
+    element.destroy()
+
+
+def inFocus(entry):
+    entry.configure(border_color='#0652DD')
+
+
+def outFocus(entry):
+    entry.configure(border_color='#dcdde1')
+
+
+def increase(btn, color, image):
+    btn.configure(width=150, height=60, fg_color=color, text_color='#f5f6fa', image=loadIcon(image, 40))
+
+
+def decrease(btn, color, image):
+    btn.configure(width=120, height=50, fg_color=color, text_color='#1e272e', image=loadIcon(image, 40))
+
+
+def searchImage():
+    tempPath = filedialog.askopenfile()
+    if tempPath is None:
+        messagebox.showwarning('Selecccione un archivo valido')
+        searchImage()
+    else:
+        return tempPath.name
+
+
+def save_image(path):
+    shutil.move(path, '../Media')
+
+
+def upper(entry):
+    current_text = entry.get()
+
+    if current_text != current_text.upper():
+        entry.delete(0, END)
+        entry.insert(0, current_text.upper())
